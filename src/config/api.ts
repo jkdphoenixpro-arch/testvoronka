6fd069f93c26a7f6467f1375df013eeb2be07420
jkdphoenixpro-arch <jkdphoenixpro@gmail.com>
2025-10-23 18:28:38 +0300

    api.ts loophole

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,7 +1,7 @@
 // Конфигурация API
 const API_CONFIG = {
   // URL вашего бэкенда на VDS сервере
-  BASE_URL: 'https://bright-masks-unite.loca.lt'
+  BASE_URL: 'https://1694534a735243139199759a1e54c376.loophole.site'
 };
 
 export default API_CONFIG;
