52afafee2b26654b64aa37ed7606b5ac0531964f
jkdphoenixpro-arch <jkdphoenixpro@gmail.com>
2025-10-23 03:35:26 +0300

    api.ts v2

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,7 +1,7 @@
 // Конфигурация API
 const API_CONFIG = {
   // URL вашего бэкенда на VDS сервере
-  BASE_URL: 'https://rich-dancers-give.loca.lt:3001'
+  BASE_URL: 'https://rich-dancers-give.loca.lt'
 };
 
 export default API_CONFIG;
