3003b374f9e0147db3ed3051c05966ff13cd047c
jkdphoenixpro-arch <jkdphoenixpro@gmail.com>
2025-11-05 00:24:52 +0200

    url

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,7 +1,7 @@
 const API_CONFIG = {
   BASE_URL: process.env.NODE_ENV === 'development' 
     ? 'http://localhost:3001' 
-    : 'https://9b3543ad7bbdf99f3e72e36435e3fd79.loophole.site'
+    : 'https://d5ca4cd0a7ae92c6c526546233462ba7.loophole.site'
 };
 
 export default API_CONFIG;
